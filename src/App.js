45b23ef61017e7d365b4451434956937c439b86b
Jumanazar04 <yusupovjumanazar004@gmail.com>
2024-05-21 16:49:06 +0500

    fix: add news

--- src/App.js
+++ src/App.js
@@ -9,10 +9,7 @@ import About from './components/pages/About/About';
 import Contact from './components/pages/Contact/Contact';
 import LoginPage from './components/pages/Login';
 import SingleProduct from './components/pages/SingleProduct';
-
-
-
-
+import Admin from './components/pages/Admin';
 
 function App() {
   return (
@@ -25,6 +22,7 @@ function App() {
             <Route path='/about' element={<About/>}/>
             <Route path='/contact' element={<Contact/>}/>
             <Route path='/products/:id' element={<SingleProduct />}/>
+            <Route path='/admin' element={<Admin />}/>
           </Route>
           <Route path='/login' element={<LoginPage/>}/>
         </Routes>
