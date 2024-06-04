5b78d55f4b7534be2834fd462524af5c4827fb28
Jumanazar04 <yusupovjumanazar004@gmail.com>
2024-06-04 13:54:40 +0500

    fix: add few changes

--- src/components/hooks/useMobile.jsx
+++ src/components/hooks/useMobile.jsx
@@ -1,4 +1,5 @@
-import React, { useState, useEffect } from 'react';
+import React from "react"; 
+import  { useState, useEffect } from 'react';
 
 function useMobile() {
     const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
